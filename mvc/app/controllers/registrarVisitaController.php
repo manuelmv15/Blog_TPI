3b7660e12010c98060f7143d53310fac5a28690a
manuelmv15 <mv23036@ues.edu.sv>
2025-10-13 07:47:53 -0600

    feat: cracion del controller registrar visita

--- mvc/app/controllers/registrarVisitaController.php
+++ mvc/app/controllers/registrarVisitaController.php
@@ -1,12 +1,38 @@
 <?php
 namespace app\controllers;
 use lib\controller;
+
+use app\models\PersonaModel;
+
 class registrarVisitaController extends controller{
 
     public function index(){
-        
+        $model = new PersonaModel();
+        $personas = $model->getPersona();
         return $this->view('registrarVisitaView',[
-            'title'=>'Registrar Visita']);
+            'title'=>'Registrar Visita',
+            'personas' => $personas
+        ]);
+    }
+
+    public function guardarPersona(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data = [
+                'nombre' => $_POST['nombre'],
+                'apellido' => $_POST['apellido'],
+                'edad' => $_POST['edad'],
+                'correo' => $_POST['correo']
+            ];
+            $model = new PersonaModel();
+            if ($model->guardarPersona($data)) {
+                 echo "Guardado correctamente";
+                 return $this->index();
+            }else {
+                return $this->view('error',[
+                    'title'=>'Error al guardar'
+                ]);
+            }
+        }
     }
     
 }

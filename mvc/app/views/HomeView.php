7e0c5760685b39fa8a824d8c38c39d80efe5fe8e
manuelmv15 <mv23036@ues.edu.sv>
2025-10-12 22:49:00 -0600

    feat: maquetacion del home

--- mvc/app/views/HomeView.php
+++ mvc/app/views/HomeView.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
+
+    <link rel="stylesheet" href="../public/css/style.css">
 </head>
+
 <body>
-    <h1>hola</h1>
+
+    <div>
+        <div>
+            <?php
+            include __DIR__ . '/partials/navbar.php'; ?>
+
+        </div>
+        <div>
+            <h1>Bienvenido a mi blog sobre La Semana de Sistemas 2025</h1>
+            <p>En este blog compartire mis experiencias a lo largo de la semana</p>
+            <p>El blog esta creado con PHP y el patron de disenio MVC</p>
+            <p>Utilizando docker y docker-compose para la containerizacion de la aplicacion</p>
+
+        </div>
+        <div>
+            <h2>Que es la semana de sistemas?</h2>
+            <p>La semana de sistema es una evento llevado anio con anio por la carrera de Ingenieria de Sistemas Informaticos, esta esta organisada por la asicociacion ASEIS.</p>
+            <p>Durante la semana se llevan acabo diversas actividades como lo son: ponencias, talleres y actividades recreativas</p>
+        </div>
+        <div>
+            <h2>Actividades que se llevaron acabo</h2>
+            <ul>
+                <li>Lunes: Bienvenida, ponencia y Hackathon (C#)</li>
+                <li>Martes: Talleres </li>
+                <li>Miercoles: Torneo de futbol 11 </li>
+                <li>Jueves: Feria de logros y Comelona de pupusas</li>
+                <li>Viernes: Bingo, Torneo de videojuegos y Fiesta</li>
+            </ul>
+    </div>
+
 </body>
+
 </html>
---
+++ mvc/app/views/partials/navbar.php
@@ -0,0 +1,8 @@
+ <nav>
+        <a href="">Home</a>
+        <a href="">Lunes</a>
+        <a href="">Martes</a>
+        <a href="">Miercoles</a>
+        <a href="">Jueves</a>
+        <a href="">Viernes</a>
+</nav>
